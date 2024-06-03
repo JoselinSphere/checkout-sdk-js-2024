67d8a0687c22d87380414dba36546f808769b1cd
mesajil-sphere <luis.mesajil@sphere.com.pe>
2024-06-03 14:07:23 -0500

    feat: Update CulqiPaymentStrategy execution to submit order

--- packages/core/src/payment/strategies/Culqi/culqi-payment-strategy.ts
+++ packages/core/src/payment/strategies/Culqi/culqi-payment-strategy.ts
@@ -2,14 +2,15 @@
 import { PaymentInitializeOptions } from '@bigcommerce/checkout-sdk/payment-integration-api';
 import PaymentStrategy from '../payment-strategy';
 import { CheckoutStore, InternalCheckoutSelectors } from '../../../checkout';
-import { OrderRequestBody } from '../../../order';
+import { OrderActionCreator, OrderRequestBody } from '../../../order';
 import { PaymentRequestOptions } from '../../payment-request-options';
 import { OrderFinalizationNotRequiredError } from '../../../order/errors';
+import { PaymentArgumentInvalidError } from '../../errors';
 
 export default class CulqiPaymentStrategy implements PaymentStrategy {
     constructor(
         private _store: CheckoutStore,
-        // private _orderActionCreator: OrderActionCreator,
+        private _orderActionCreator: OrderActionCreator,
     ) {}
 
    /* initialize(options: PaymentInitializeOptions): Promise<InternalCheckoutSelectors> {
@@ -43,8 +44,35 @@ export default class CulqiPaymentStrategy implements PaymentStrategy {
         return Promise.resolve(this._store.getState());
     }
 
-    async execute(_payload: OrderRequestBody, _options?: PaymentRequestOptions): Promise<InternalCheckoutSelectors> {
+    async execute(payload: OrderRequestBody, options?: PaymentRequestOptions): Promise<InternalCheckoutSelectors> {
         // Implementar lógica de ejecución de pago
+        const { payment, ...order } = payload;
+
+        if (!payment) {
+            throw new PaymentArgumentInvalidError(['payment']);
+        }
+
+        // Primero, envía el pedido a BigCommerce
+        await this._store.dispatch(this._orderActionCreator.submitOrder(order, options));
+
+        // try {
+        //     // Obtén el token de Culqi usando los detalles del pago
+        //     const culqiToken = await getCulqiToken(payment);
+
+        //     // Procesa el pago usando el token de Culqi
+        //     const paymentResult = await processCulqiPayment(culqiToken);
+
+        //     if (paymentResult.success) {
+        //         // Si el pago es exitoso, actualiza el estado de la tienda y retorna
+        //         return this._store.getState();
+        //     } else {
+        //         // Maneja el error del pago aquí
+        //         throw new Error('Error procesando el pago con Culqi');
+        //     }
+        // } catch (error) {
+        //     // Maneja cualquier error ocurrido durante el procesamiento del pago
+        //     throw error;
+        // }
         return Promise.resolve(this._store.getState());
     }
 
